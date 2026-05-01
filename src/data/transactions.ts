fb68bad09f2fd8a382b1c620673f00164998a3e4
alishbatariq54-byte <alishbatariq54@gmail.com>
2026-05-01 23:46:39 +0500

    Add files via upload

---
+++ src/data/transactions.ts
@@ -0,0 +1,4 @@
+export const transactions = [
+  { amount: 500, sender: "Investor", receiver: "Startup", status: "Success" },
+  { amount: 300, sender: "User", receiver: "Wallet", status: "Pending" },
+];
